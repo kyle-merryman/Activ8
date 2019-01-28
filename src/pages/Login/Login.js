1519ed8c19c244aca9158ae6abee508cd695bd4f
Justin Singh <justin.singh125125@gmail.com>
2019-01-28 02:43:21 -0800

    test

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -14,6 +14,7 @@ export default class Login extends Component {
         })
     }
     render() {
+        // test
         return (
             <div className="center-this" >
                 <a href="/auth/google">
